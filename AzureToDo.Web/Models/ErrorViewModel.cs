fee552def09dc566475a7a109736fafb8e813091
Tony Mocella <Tony.Mocella@neudesic.com>
2024-01-05 07:45:18 -0600

    Add project files.

---
+++ AzureToDo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureToDo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
