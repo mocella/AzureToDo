e0e40d39253050e8febe5e46ff9eabb3f3c56963
Tony Mocella <Tony.Mocella@neudesic.com>
2024-01-09 13:49:40 -0600

    now with fluent migrations

---
+++ AzureToDo.DBMigrations/Migrations/20240109_0001_CreateSupportTicket.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace AzureToDo.DBMigrations.Migrations
+{
+    [Migration(202401090001)]
+    public class CreateSupportTicket : Migration
+    {
+        private const string TableName = "SupportTickets";
+
+        public override void Up()
+        {
+            this.CreateTableIfNotExists(TableName,
+                table =>
+                    table
+                        .WithColumn("Id").AsInt32().NotNullable().PrimaryKey("PK_SupportTicket_Id").Identity()
+                        .WithColumn("Title").AsString()
+                        .WithColumn("Description").AsString()
+                    );
+        }
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+    }
+}
--- AzureToDo.DBMigrations/Program.cs
+++ AzureToDo.DBMigrations/Program.cs
@@ -1,6 +1,8 @@
 using AzureToDo.Db.Entities;
 using AzureToDo.DBMigrations;
+using FluentMigrator.Runner;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +15,21 @@ builder.AddNpgsqlDbContext<TicketContext>("ticketdb", null,
 builder.Services.AddOpenTelemetry()
     .WithTracing(tracing => tracing.AddSource(TicketDbInitializer.ActivitySourceName));
 
-builder.Services.AddSingleton<TicketDbInitializer>();
-builder.Services.AddHostedService(sp => sp.GetRequiredService<TicketDbInitializer>());
+builder.Services.Configure<ConnectionStringConfig>(builder.Configuration.GetSection("ConnectionStrings"));
+builder.Services.AddFluentMigratorCore()
+        .ConfigureRunner(rb => rb
+               .AddPostgres()
+                      .WithGlobalConnectionString(builder.Configuration.GetConnectionString("ticketdb"))
+                             .ScanIn(typeof(Program).Assembly).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole());
+
+builder.Services.AddScoped<TicketDbInitializer>();
+builder.Services.AddHostedService(sp =>
+{
+    using var scope = sp.CreateScope();
+    return scope.ServiceProvider.GetRequiredService<TicketDbInitializer>();
+});
+
 builder.Services.AddHealthChecks()
     .AddCheck<TicketDbInitializerHealthCheck>("DbInitializer", null);
 
